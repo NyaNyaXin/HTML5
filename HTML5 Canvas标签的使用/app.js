148d673a1d35f6078412e314d8b8f53e5cb6a4ab
NyaNyaXin <chenxin95013>
2017-02-19 17:01:49 +0800

    Day16 Canvas

--- HTML5 Canvas标签的使用/app.js
+++ HTML5 Canvas标签的使用/app.js
@@ -2,12 +2,23 @@
  * Created by qq960 on 2017/2/19.
  */
 var CANVAS_WIDTH = 500 , CANVAS_HEIGHT = 500;
-
+var mycanvas , context;
 window.onload = function () {
     createCanvas();
+    drawRect();
 }
     function createCanvas() {
         document.body.innerHTML = "<canvas id=\"mycanvas\" " +
             "width=\""+CANVAS_WIDTH+"\" " +
-            "height=\""+CANVAS_HEIGHT+"\"></canvas></canvas>";
+            "height=\""+CANVAS_HEIGHT+"\"></canvas></canvas>"
+        mycanvas = document.getElementById("mycanvas");
+        context = mycanvas.getContext("2d");
+    }
+    
+    function drawRect() {
+        context.fillStyle = "#FF0000";//设置颜色
+        // context.rotate(45); //设置绘制图形的旋转角度
+        //context.translate(200,200); //设置图形的移动位置
+        context.scale(2,0.5); //缩放
+        context.fillRect(0,0,200,200);//设置绘制图形的位置大小等
     }
