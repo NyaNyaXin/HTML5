0cba78ce88c0c3cd2d3890fcab7383b5a0fb9e18
NyaNyaXin <chenxin95013>
2017-02-19 16:01:02 +0800

    Day16

--- HTML5 Canvas标签的使用/app.js
+++ HTML5 Canvas标签的使用/app.js
@@ -1,3 +1,13 @@
 /**
  * Created by qq960 on 2017/2/19.
  */
+var CANVAS_WIDTH = 500 , CANVAS_HEIGHT = 500;
+
+window.onload = function () {
+    createCanvas();
+}
+    function createCanvas() {
+        document.body.innerHTML = "<canvas id=\"mycanvas\" " +
+            "width=\""+CANVAS_WIDTH+"\" " +
+            "height=\""+CANVAS_HEIGHT+"\"></canvas></canvas>";
+    }
